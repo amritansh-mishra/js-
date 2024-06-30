a0910e522c936cfe9be70f8b27a7d37d1ce71e23
amritansh-mishra <mamritansh1911@gmail.com>
2024-06-30 12:59:33 +0530

    new file:   01_basics/Operations.js 	new file:   01_basics/comparison.js 	new file:   01_basics/conversions.js 	modified:   01_basics/test.js

---
+++ 01_basics/comparison.js
@@ -0,0 +1,20 @@
+/* console.log(2>1);
+   console.log(2>=1);
+   console.log(2<1);
+   console.log(2==1);
+   console.log(2!=1);
+*/
+
+console.log("2" >1);
+console.log("02" >1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+//comparison converts null to a number treating it as a zero.
+
+// that's why  null >= 0 true and null > 0 false .
+
+// Note (===) check data type 
+
---
+++ 01_basics/conversions.js
@@ -0,0 +1,24 @@
+let score = "33ab" 
+
+//"33" = 33
+//"33abc" = NaN
+// true = 1 false = 0
+
+console.log(typeof score);
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+let isloggedIn = "hitesh"
+let booleanIsloggedin = Boolean(isloggedIn);
+console.log(booleanIsloggedin);
+
+// 1 = true
+// "" = false
+//"hitesh" = true
+
+
+
+
+
