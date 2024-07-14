e86e82d77053e364b62cb6f8f52f22aed3617fa6
amritansh-mishra <mamritansh1911@gmail.com>
2024-07-14 11:48:09 +0530

    new file:   01_basics/dates.js

---
+++ 01_basics/dates.js
@@ -0,0 +1,25 @@
+let myDate = new Date()
+console.log(myDate.toString());       // Sun Jul 14 2024 11:36:28 GMT+0530 (India Standard Time)
+
+console.log(myDate.toDateString());  //Sun Jul 14 2024
+
+console.log(myDate.toLocaleString()); //7/14/2024, 11:36:28 AM
+
+let mycreatedDate = new Date(2023 , 0 ,23)
+console.log(mycreatedDate.toDateString());
+//Mon Jan 23 2023
+//Months starts with 0 in java script
+
+let mycreatedDate2 = new Date("01-14-2023")
+console.log(mycreatedDate2.toLocaleString());
+//1/14/2023, 12:00:00 AM
+
+
+
+
+ 
+
+
+
+
+
