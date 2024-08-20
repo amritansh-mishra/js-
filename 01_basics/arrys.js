8e5f68f08746340ae3c5b81eda837b77ceb35833
amritansh-mishra <mamritansh1911@gmail.com>
2024-08-21 01:09:05 +0530

    modified:   01_basics/arrys.js

--- 01_basics/arrys.js
+++ 01_basics/arrys.js
@@ -1,6 +1,18 @@
 const myArr=[0,1,2,3,5,6]
 const myheroes = ["shaktiman",'naagraj']
 
+const myArr2 = new Array(1, 2, 3, 4)
+
+console.log(myArr[1]);
+
+// Array methods
+
+ myArr.push(6)
+ myArr.push(7)  // adds value to arryas
+ console.log(myArr);
+
+ 
+
 
 
 
